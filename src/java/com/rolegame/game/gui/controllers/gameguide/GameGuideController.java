8d5a21ee333466e0c21127d8704498cf1bcee199
Furkan Kırat <furkankirat04@gmail.com>
2025-01-28 22:38:13 +0300

    Auto stash before merge of "main" and "origin/main"

--- src/java/com/rolegame/game/gui/controllers/gameguide/GameGuideController.java
+++ src/java/com/rolegame/game/gui/controllers/gameguide/GameGuideController.java
@@ -2,6 +2,7 @@ package com.rolegame.game.gui.controllers.gameguide;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -11,15 +12,16 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GameGuideController {
+public class GameGuideController implements Initializable {
 
     @FXML
     private BorderPane borderPane;
 
     @FXML
     private VBox changingPane;
-
 
     @FXML
     private HBox gameRulesHBox;
@@ -96,4 +98,9 @@ public class GameGuideController {
         }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        changingPane.prefWidthProperty().bind(borderPane.widthProperty().multiply(0.5));
+        changingPane.prefHeightProperty().bind(borderPane.heightProperty().multiply(0.5));
+    }
 }
